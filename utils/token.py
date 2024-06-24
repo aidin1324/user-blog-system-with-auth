af0a570fd25382b451035d868c3adfb0e530ae07
Aidin <aidinror@gmail.com>
2024-06-24 14:59:44 +0600

    finish oauth2

---
+++ utils/token.py
@@ -0,0 +1,15 @@
+# hash and verify password
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+
